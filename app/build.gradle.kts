5279a5191c78f8f09f064ab7218f4fec5e712ab2
RD2W <mkrutovercev@yandex.ru>
2024-11-28 21:03:00 +0300

    Повысить версию приложения до 1.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.practicum.playlistmaker"
         minSdk = 29
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
