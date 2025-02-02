e20df88acd46740dcb320956b7f926979fb51ad2
RD2W <mkrutovercev@yandex.ru>
2025-02-02 16:18:00 +0300

    Использовать data object для состояний экрана в PlayerScreenState

--- app/src/main/java/com/practicum/playlistmaker/player/presentation/state/PlayerScreenState.kt
+++ app/src/main/java/com/practicum/playlistmaker/player/presentation/state/PlayerScreenState.kt
@@ -1,9 +1,9 @@
 package com.practicum.playlistmaker.player.presentation.state
 
 sealed class PlayerScreenState {
-    object Ready : PlayerScreenState()
-    object NotReady : PlayerScreenState()
-    object Playing : PlayerScreenState()
-    object Paused : PlayerScreenState()
-    object Stopped : PlayerScreenState()
+    data object Ready : PlayerScreenState()
+    data object NotReady : PlayerScreenState()
+    data object Playing : PlayerScreenState()
+    data object Paused : PlayerScreenState()
+    data object Stopped : PlayerScreenState()
 }
