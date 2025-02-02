8b9ec8b8f5da26d433aa56c7427d34a7c3e9808f
RD2W <mkrutovercev@yandex.ru>
2025-02-02 16:24:31 +0300

    Использовать data object для состояний экрана в SettingsScreenState

--- app/src/main/java/com/practicum/playlistmaker/settings/presentation/state/SettingsScreenState.kt
+++ app/src/main/java/com/practicum/playlistmaker/settings/presentation/state/SettingsScreenState.kt
@@ -2,5 +2,5 @@ package com.practicum.playlistmaker.settings.presentation.state
 
 sealed class SettingsScreenState {
     data class ThemeSwitcherState(val isThemeChecked: Boolean) : SettingsScreenState()
-    object Error : SettingsScreenState()
+    data object Error : SettingsScreenState()
 }
