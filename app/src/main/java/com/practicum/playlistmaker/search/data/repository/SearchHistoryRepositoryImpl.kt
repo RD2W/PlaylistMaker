57d19dc69f89603a2bb627f2e5e9b2f415e01e71
RD2W <mkrutovercev@yandex.ru>
2025-02-03 23:13:33 +0300

    SearchHistoryRepositoryImpl: использовать абстракцию через интерфейс LocalClient

--- app/src/main/java/com/practicum/playlistmaker/search/data/repository/SearchHistoryRepositoryImpl.kt
+++ app/src/main/java/com/practicum/playlistmaker/search/data/repository/SearchHistoryRepositoryImpl.kt
@@ -1,22 +1,13 @@
 package com.practicum.playlistmaker.search.data.repository
 
 import com.practicum.playlistmaker.common.domain.model.Track
-import com.practicum.playlistmaker.search.data.source.local.sprefs.SharedPreferencesClient
+import com.practicum.playlistmaker.search.data.source.local.LocalClient
 import com.practicum.playlistmaker.search.domain.repository.SearchHistoryRepository
 
 class SearchHistoryRepositoryImpl(
-    private val sharedPreferencesClient: SharedPreferencesClient,
+    private val localClient: LocalClient,
 ) : SearchHistoryRepository {
-
-    override fun getHistory(): List<Track> {
-        return sharedPreferencesClient.getHistory()
-    }
-
-    override fun addTrack(track: Track) {
-        sharedPreferencesClient.addTrack(track)
-    }
-
-    override fun clearHistory() {
-        sharedPreferencesClient.clearHistory()
-    }
+    override fun addTrack(track: Track) = localClient.addTrack(track)
+    override fun getHistory(): List<Track> = localClient.getHistory()
+    override fun clearHistory() = localClient.clearHistory()
 }
