b3486903b986b5723ce71913094e87e50f86f2b9
RD2W <mkrutovercev@yandex.ru>
2025-02-04 20:58:22 +0300

    Подключить библиотеку Koin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,8 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.gson)
     implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.koin.core)
+    implementation(libs.koin.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
