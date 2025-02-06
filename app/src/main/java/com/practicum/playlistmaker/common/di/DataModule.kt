67c0fe31b8f3039fcdbc5da81e3f176f759b3c66
RD2W <mkrutovercev@yandex.ru>
2025-02-06 13:50:41 +0300

    Передавать ITunesApiService в RetrofitClient через конструктор класса

--- app/src/main/java/com/practicum/playlistmaker/common/di/DataModule.kt
+++ app/src/main/java/com/practicum/playlistmaker/common/di/DataModule.kt
@@ -3,6 +3,7 @@ package com.practicum.playlistmaker.common.di
 import android.content.Context
 import androidx.media3.exoplayer.ExoPlayer
 import com.google.gson.Gson
+import com.practicum.playlistmaker.common.constants.ApiConstants
 import com.practicum.playlistmaker.common.constants.PrefsConstants
 import com.practicum.playlistmaker.common.data.repository.AppThemeRepositoryImpl
 import com.practicum.playlistmaker.common.domain.repository.AppThemeRepository
@@ -12,6 +13,8 @@ import com.practicum.playlistmaker.search.data.repository.SearchHistoryRepositor
 import com.practicum.playlistmaker.search.data.repository.TracksRepositoryImpl
 import com.practicum.playlistmaker.search.data.source.local.LocalClient
 import com.practicum.playlistmaker.search.data.source.local.sprefs.SharedPreferencesClient
+import com.practicum.playlistmaker.search.data.source.remote.ITunesApiService
+import com.practicum.playlistmaker.search.data.source.remote.NetworkClient
 import com.practicum.playlistmaker.search.data.source.remote.RetrofitClient
 import com.practicum.playlistmaker.search.domain.repository.SearchHistoryRepository
 import com.practicum.playlistmaker.search.domain.repository.TracksRepository
@@ -22,6 +25,8 @@ import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val dataModule = module {
     single {
@@ -30,12 +35,26 @@ val dataModule = module {
             Context.MODE_PRIVATE
         )
     }
+
+    single {
+        Retrofit.Builder()
+            .baseUrl(ApiConstants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    single<ITunesApiService> {
+        get<Retrofit>().create(ITunesApiService::class.java)
+    }
+
     factory { ExoPlayer.Builder(androidApplication()).build() }
-    single<TracksRepository> { TracksRepositoryImpl(get(), RetrofitClient) }
+
     singleOf(::Gson)
-    singleOf(::SharedPreferencesClient) { bind<LocalClient>() }
     singleOf(::AppThemeRepositoryImpl) { bind<AppThemeRepository>() }
     factoryOf(::PlayerRepositoryImpl) { bind<PlayerRepository>() }
+    singleOf(::RetrofitClient) { bind<NetworkClient>() }
+    singleOf(::SharedPreferencesClient) { bind<LocalClient>() }
     singleOf(::SettingsRepositoryImpl) { bind<SettingsRepository>() }
     singleOf(::SearchHistoryRepositoryImpl) { bind<SearchHistoryRepository>() }
+    singleOf(::TracksRepositoryImpl) { bind<TracksRepository>() }
 }
