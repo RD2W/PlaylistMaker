9141267d03ee482c5075331e350394afcdd7c129
RD2W <mkrutovercev@yandex.ru>
2025-03-18 20:18:31 +0300

    Задать время обновления прогресс бара воспроизведения трека равным 300 мс

--- app/src/main/java/com/practicum/playlistmaker/common/constants/AppConstants.kt
+++ app/src/main/java/com/practicum/playlistmaker/common/constants/AppConstants.kt
@@ -6,6 +6,6 @@ object AppConstants {
     const val NOT_AVAILABLE = "N/A"
     const val CLICK_DEBOUNCE_DELAY_MILLIS = 1000L
     const val SEARCH_DEBOUNCE_DELAY_MILLIS = 2000L
-    const val PROGRESS_BAR_DELAY_MILLIS = 500L
+    const val PROGRESS_BAR_DELAY_MILLIS = 300L
     const val START_TRACK_POSITION = "00:00"
 }
